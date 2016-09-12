3d9dee9494c3ed4e35db803ebf1704a367543e5f
johnn134 <johnn134@gmail.com>
2016-09-12 15:40:13 +0900

    Add Clock Object
    Add Clock Object that displays the remaining time left during the level

---
+++ Assets/Clock/Clock.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	public float gameMinutes;
+	public float gameSeconds;
+
+	public bool isActive = false;
+
+	float startTime;
+	float gameDuration;
+
+	void Awake() {
+		gameDuration = (gameMinutes * 60.0f) + gameSeconds;
+	}
+
+	// Use this for initialization
+	void Start () {
+		startClock();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(isActive) {
+			//Minute Hand
+			transform.GetChild(0).GetChild(0).localRotation = Quaternion.Euler(new Vector3(0.0f, 360.0f * ((Time.time - startTime) / gameDuration), 0.0f));
+
+			//check for gameover
+			if(Time.time > gameDuration + startTime) {
+				endLevel();
+			}
+		}
+	}
+
+	public void startClock() {
+		startTime = Time.time;
+		isActive = true;
+	}
+
+	void endLevel() {
+		isActive = false;
+
+		//End the level if time is up
+	}
+}

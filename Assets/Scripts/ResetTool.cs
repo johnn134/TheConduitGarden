67c0158da5956710525aa42884ba51ce57c9ee86
Alex Guerra <silverx@verizon.net>
2016-09-12 14:10:46 +0900

    Make reset box detect tool collision and place them back above the tool table

---
+++ Assets/Scripts/ResetTool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetTool : MonoBehaviour {
+
+    public GameObject target;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Tool"))
+        {
+            other.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 1, target.transform.position.z);
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+}

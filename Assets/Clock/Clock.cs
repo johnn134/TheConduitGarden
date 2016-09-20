eb07baf8b5a4f66418889392b41103860118acb3
johnn134 <johnn134@gmail.com>
2016-09-20 14:51:53 +0900

    Make Clock end game at time end

--- Assets/Clock/Clock.cs
+++ Assets/Clock/Clock.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Clock : MonoBehaviour {
 
+	public GameObject kamimanager;
+
 	public float gameMinutes;
 	public float gameSeconds;
-
-	public bool isActive = false;
 
 	float startTime;
 	float gameDuration;
 
+	public bool isActive = false;
+
+	bool isEnding;
+
+	const float KAMI_FLEE_TIME = 10.0f;
+
+	const string HOME_LEVEL = "Home";
+
 	void Awake() {
+		isEnding = false;
 		gameDuration = (gameMinutes * 60.0f) + gameSeconds;
 	}
 
@@ -24,12 +34,19 @@ public class Clock : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if(isActive) {
-			//Minute Hand
-			transform.GetChild(0).GetChild(0).localRotation = Quaternion.Euler(new Vector3(0.0f, 360.0f * ((Time.time - startTime) / gameDuration), 0.0f));
+			if(!isEnding) {
+				//Minute Hand
+				transform.GetChild(0).GetChild(0).localRotation = Quaternion.Euler(new Vector3(0.0f, 360.0f * ((Time.time - startTime) / gameDuration), 0.0f));
 
-			//check for gameover
-			if(Time.time > gameDuration + startTime) {
-				endLevel();
+				//check for gameover
+				if(Time.time > startTime + gameDuration) {
+					startLevelEnd();
+				}
+			}
+			else {
+				if(Time.time > startTime + gameDuration + KAMI_FLEE_TIME) {
+					endLevel();
+				}
 			}
 		}
 	}
@@ -39,9 +56,15 @@ public class Clock : MonoBehaviour {
 		isActive = true;
 	}
 
+	void startLevelEnd() {
+		isEnding = true;
+		kamimanager.GetComponent<KamiManager>().MakeKamiEnd();
+	}
+
 	void endLevel() {
 		isActive = false;
 
 		//End the level if time is up
+		SceneManager.LoadScene(HOME_LEVEL);
 	}
 }

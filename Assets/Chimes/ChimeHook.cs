8d228b034e384e6bec04318ac353e2a2f3651f12
johnn134 <johnn134@gmail.com>
2016-09-26 16:41:20 +0900

    Add working chimes

---
+++ Assets/Chimes/ChimeHook.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChimeHook : MonoBehaviour {
+
+	GameObject attachedChime;
+
+	/*
+	 * sets this hook's focus to the given chime gameobject
+	 */
+	public void attachChime(GameObject g) {
+		attachedChime = g;
+	}
+
+	/*
+	 * nullifies the focused hook
+	 */
+	public void removeChime() {
+		attachedChime = null;
+	}
+
+	/*
+	 * plays the note on the attached hook if present
+	 */
+	public void playChime() {
+		if(attachedChime != null)
+			attachedChime.GetComponent<WindChime>().playNote();
+	}
+}
---
+++ Assets/Chimes/ChimeManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChimeManager : MonoBehaviour {
+
+	public GameObject[] hooks;
+
+	public float loopTime;	//time offset for looping chime notes
+
+	float lastPlayTime;		//last time at the end of a loop cycle
+
+	int curHook;			//current hook to be activated
+
+	const float CHIME_PLAY_OFFSET = 0.25f;
+
+	public static ChimeManager instance = null;
+
+	void Awake() {
+		//declare as singleton
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		lastPlayTime = 0;
+		curHook = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//after the wait time between loops, play the note on each hook's chime
+		if(Time.time > lastPlayTime + loopTime + CHIME_PLAY_OFFSET * curHook) {
+			hooks[curHook].GetComponent<ChimeHook>().playChime();
+			curHook++;
+
+			if(curHook >= hooks.Length) {	//reset the wait time between loops
+				lastPlayTime = Time.time;
+				curHook = 0;
+			}
+		}
+	}
+}

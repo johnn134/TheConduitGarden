6250271555f64420d3ff1d51cd65d300b3914549
POrvis5 <pworvis@wpi.edu>
2016-09-28 21:55:14 +0900

    Pattern Recognition Fix and Clearing Wind
    Pattern recognition fixed and sections of the gravel pit are cleared
    with wind.

--- Assets/GravelPit/PatternLine.cs
+++ Assets/GravelPit/PatternLine.cs
@@ -4,10 +4,7 @@ using System.Collections;
 public class PatternLine : MonoBehaviour {
 	public GameObject patternStartNode;
 	public GameObject patternEndNode;
-	public Vector3 patternVectorFromStart;
-	public Vector3 patternVectorFromEnd;
+	public Vector3 patternVector;
 	public bool isVertical;
-	public bool wasChecked;
-	public int patternLineNumber;
 	public int patternDimension;
 }

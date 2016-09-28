29c5c621116d51fa59bcfa5208878e7e968bcc82
johnn134 <johnn134@gmail.com>
2016-09-28 19:14:30 +0900

    Make scroll dragging Orientation dependent

--- Assets/Contract/Scroll/ScrollDrag.cs
+++ Assets/Contract/Scroll/ScrollDrag.cs
@@ -5,10 +5,16 @@ public class ScrollDrag : MonoBehaviour {
 
 	public GameObject target;
 
+	public SCROLLTYPE scrollType;
+
 	bool grabbed;
 
 	const float UPPER_LIMIT = 0.9f;
 	const float LOWER_LIMIT = -0.95f;
+
+	public enum SCROLLTYPE {
+		VERTICAL, HORIZONTAL
+	};
 
 	// Use this for initialization
 	void Start () {
@@ -82,21 +88,46 @@ public class ScrollDrag : MonoBehaviour {
 			Transform visualBottom = transform.parent.GetChild(0).GetChild(1);
 			Vector3 visualTopPos = transform.parent.GetChild(0).GetChild(0).position;
 			Vector3 otherPos = other.transform.position;
-			float offset = visualBottom.position.y - transform.position.y;	//between bottom visual and drag point
+			float offset;
 
-			//reposition the visual of the bottom of the scroll object
-			visualBottom.position = new Vector3(visualBottom.position.x, 
-				Mathf.Clamp(otherPos.y + offset, 
-					visualTopPos.y + LOWER_LIMIT * transform.parent.lossyScale.y, 
-					visualTopPos.y + UPPER_LIMIT * transform.parent.lossyScale.y), 
-				visualBottom.position.z);
-			
-			//reposition this point
-			transform.position = new Vector3(transform.position.x, 
-				Mathf.Clamp(otherPos.y, 
-					visualTopPos.y + LOWER_LIMIT * transform.parent.lossyScale.y - offset, 
-					visualTopPos.y + UPPER_LIMIT * transform.parent.lossyScale.y - offset), 
-				transform.position.z);
+			switch(scrollType) {
+				case SCROLLTYPE.VERTICAL:
+					offset = visualBottom.position.y - transform.position.y;	//between bottom visual and drag point
+
+					//reposition the visual of the bottom of the scroll object
+					visualBottom.position = new Vector3(visualBottom.position.x, 
+						Mathf.Clamp(otherPos.y + offset, 
+							visualTopPos.y + LOWER_LIMIT * transform.parent.lossyScale.y, 
+							visualTopPos.y + UPPER_LIMIT * transform.parent.lossyScale.y), 
+						visualBottom.position.z);
+					
+					//reposition this point
+					transform.position = new Vector3(transform.position.x, 
+						Mathf.Clamp(otherPos.y, 
+							visualTopPos.y + LOWER_LIMIT * transform.parent.lossyScale.y - offset, 
+							visualTopPos.y + UPPER_LIMIT * transform.parent.lossyScale.y - offset), 
+						transform.position.z);
+					
+					break;
+				case SCROLLTYPE.HORIZONTAL:
+					offset = visualBottom.position.x - transform.position.x;	//between bottom visual and drag point
+
+					//reposition the visual of the bottom of the scroll object
+					visualBottom.position = new Vector3(Mathf.Clamp(otherPos.x + offset, 
+							visualTopPos.x + LOWER_LIMIT * transform.parent.lossyScale.y, 
+							visualTopPos.x + UPPER_LIMIT * transform.parent.lossyScale.y), 
+						visualBottom.position.y, 
+						visualBottom.position.z);
+
+					//reposition this point
+					transform.position = new Vector3(Mathf.Clamp(otherPos.x, 
+							visualTopPos.x + LOWER_LIMIT * transform.parent.lossyScale.y - offset, 
+							visualTopPos.x + UPPER_LIMIT * transform.parent.lossyScale.y - offset),
+						transform.position.y,
+						transform.position.z);
+
+					break;
+			}
 		}								 
 	}
 }
